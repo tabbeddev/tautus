922b10c95d8f786bafa8f4a52f61626c97f44f7c
tabbeddev <83753407+tabbeddev@users.noreply.github.com>
2026-02-02 11:20:29 +0100

    Fixed broken imports from python-libs

--- src/tautus/commands/shell.py
+++ src/tautus/commands/shell.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 from pathlib import Path
 
@@ -8,10 +9,17 @@ def shell(command: str | None):
     venv_path = Path("tautus-venv").absolute()
     venv_python = venv_path / "bin" / "python"
 
+    env = os.environ.copy()
+
+    if "PYTHONPATH" not in env:
+        env["PYTHONPATH"] = ""
+
+    env["PYTHONPATH"] += ":" + os.path.abspath("python-libs")
+
     args = [venv_python]
     if command:
         args += ["-c", command]
 
-    result = subprocess.run(args, text=True)
+    result = subprocess.run(args, text=True, env=env)
 
     handle_run_error(result, "The called python instance exited with an error")
