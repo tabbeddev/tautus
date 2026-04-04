473e5947630ad1434dc5e8961a6759085a368375
tabbeddev <83753407+tabbeddev@users.noreply.github.com>
2026-04-04 23:07:38 +0200

    build: Add pycache cleanup function

--- build.py
+++ build.py
@@ -1,16 +1,32 @@
 #!/usr/bin/env python3
-import os
+from pathlib import Path
+from shutil import rmtree
 import stat
 import zipapp
 
+
+def traverse_directory(path: Path):
+    for content in path.iterdir():
+        if content.is_dir():
+            if content.name == "__pycache__":
+                print("Deleting pycache: ", content)
+                rmtree(content)
+            else:
+                traverse_directory(content)
+
+
+traverse_directory(Path("src").absolute())
+
+target = Path("tautus.pyz").absolute()
+
 zipapp.create_archive(
     source="src",
-    target="tautus.pyz",
+    target=target,
     main="tautus.__main__:main",
     interpreter="/usr/bin/env python3",
     compressed=True,
 )
-print("Create tautus.pyz")
+print("Created", target.name)
 
-st = os.stat("tautus.pyz")
-os.chmod("tautus.pyz", st.st_mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
+st = target.stat()
+target.chmod(st.st_mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
